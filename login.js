4bf5e55fd52767419946a5aab9599917d8b4c600
Charlie Pontrelli <cpontrelli12@gmail.com>
2021-06-05 22:37:56 -0700

    Added test suite and updated documentation

--- login.js
+++ login.js
@@ -45,7 +45,7 @@ module.exports = function(){
             clientID: process.env.AUTH0_CLIENT_ID,
             clientSecret: process.env.AUTH0_CLIENT_SECRET,
             callbackURL:
-                process.env.AUTH0_CALLBACK_URL || 'http://localhost:8080/callback'
+                process.env.AUTH0_CALLBACK_URL || 'https://cs-493-cpontrelli-final.wl.r.appspot.com/callback'
         },
         function (accessToken, refreshToken, extraParams, profile, done) {
             // accessToken is the token to call Auth0 API (not needed in the most cases)
